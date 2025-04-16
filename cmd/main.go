c5242cc015889be487f92b1b049a32c44e2521d7
sujeethcodes <sujeethcodes@gmail.com>
2025-04-16 12:33:57 +0530

    add-routing

--- cmd/main.go
+++ cmd/main.go
@@ -5,24 +5,36 @@ import (
 	"os"
 
 	"e-commerce-return-tracker/connectors"
+	"e-commerce-return-tracker/controller"
 
 	"e-commerce-return-tracker/repository"
 
 	"github.com/labstack/echo/v4"
 )
 
+type container struct {
+	returnProductInstance controller.ReturnProduct
+}
+
 func init() {
 	connectors.LoadEnv()
 }
 
+func LoadContainer() *container {
+	return &container{
+		returnProductInstance: controller.ReturnProduct{Mysql: repository.SingleMysqlConnection()},
+	}
+}
+
 func main() {
 	fmt.Println("Main func created")
-	repository.SingleMysqlConnection()
+	containerInstance := LoadContainer()
 	e := echo.New()
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "9001"
 	}
+	e.POST("/return-products", containerInstance.returnProductInstance.ReturnProducts)
 	e.Logger.Fatal(e.Start(":" + port))
 
 }
